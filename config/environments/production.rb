741645086bdcae74fe4ec77b26a759314f6a10b5
“Brian <brian_mcguire@yahoo.com>
2021-01-31 23:29:58 -0600

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
    }
 
    config.action_mailer.delivery_method = :smtp
-   config.action_mailer.default_url_options ={:host => 'http://localhost:3000'}
+   config.action_mailer.default_url_options ={:host => 'http://www.trumpdebt.heroku.com'}
 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.perform_deliveries = true
