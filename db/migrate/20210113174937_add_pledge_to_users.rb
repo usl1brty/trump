5d4ea414eddddff277e271f30f587679a92aaa3e
“Brian <brian_mcguire@yahoo.com>
2021-02-02 17:39:25 -0600

    donations

--- db/migrate/20210113174937_add_pledge_to_users.rb
+++ db/migrate/20210113174937_add_pledge_to_users.rb
@@ -1,5 +1,5 @@
 class AddPledgeToUsers < ActiveRecord::Migration[6.0]
-  # def change
-  #   change_column :users, :pledge, :integer
-  # end
+  def change
+    change_column :users, :pledge, :integer
+  end
 end
